d61cd4fd49f44dafed6ad6484f9257ee0d1a5551
vurses <vurses@qq.com>
2025-07-25 22:36:12 +0800

    chore: update preload declartion

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -4,8 +4,18 @@ declare global {
   interface Window {
     electron: ElectronAPI
     api: {
+      /** 应用窗口初始化调用，初始化页面配置 */
+
       queryProxyList: () => Promise<unknown>
+      queryUserList: () => Promise<unknown>
+
+      /** 应用运行时可能会调用 */
+
+      //增加，删除代理时调用
       updateProxyList: (proxyList: unknown) => Promise<unknown>
+      // 单用户登录时调用
+      requestUserLogin: (LoginPayload: unknown) => Promise<unknown>
+      //
     }
   }
 }
